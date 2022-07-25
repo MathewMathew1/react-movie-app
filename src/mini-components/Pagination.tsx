c7fcffb9320d28589df44dfd8a97c42a010ab2b3
Mathew <mateusz.lu123@gmail.com>
2022-07-25 17:30:26 +0200

    fixed pagination hiding before footer

--- src/mini-components/Pagination.tsx
+++ src/mini-components/Pagination.tsx
@@ -10,34 +10,23 @@ const PaginationComponent  = ({link, numberOfPages, currentPage}: {link: string,
     const restOfPagination = () => {
         
         let elements: any[] = [];
-        console.log(numberOfPages)
         if( numberOfPages<5 ){
         
             for(let i=2; i <= numberOfPages-1; i++){
-                elements.push(<Pagination.Item  key={i} active={currentPage===i} href={link+`&page=${i}`}>{i}</Pagination.Item>);
+                elements.push(<Pagination.Item  key={`${i} page`} active={currentPage===i} href={link+`&page=${i}`}>{i}</Pagination.Item>);
             }
             return elements;
         }
-        else{
-            
+        else{        
             if(currentPage<3 || numberOfPages===currentPage){
                 
-                
                 for(let i=2; i <= 5; i++){
-                    if(i===numberOfPages){
-                        break
-                    }
+                    if(i===numberOfPages)break
 
-                    if(i===numberOfPages){
-                        break
-                    }
-    
                     let className: string = '' 
-                    if(i!==currentPage){
-                        className = "hide-on-small-screens"
-                    }
+                    if(i!==currentPage) className = "hide-on-small-screens"
 
-                    elements.push(<Pagination.Item className={className} key={i} active={currentPage===i} href={link+`&page=${i}`}>{i}</Pagination.Item>);
+                    elements.push(<Pagination.Item className={className} key={`${i} page`} active={currentPage===i} href={link+`&page=${i}`}>{i}</Pagination.Item>);
                 }
                 elements.push(<Pagination.Ellipsis />)
 
@@ -47,47 +36,30 @@ const PaginationComponent  = ({link, numberOfPages, currentPage}: {link: string,
             elements.push(<Pagination.Ellipsis />)
             for(let i=currentPage; i <= currentPage+3; i++){
                 
-                if(i===numberOfPages){
-                    break
-                }
-
-                if(i===numberOfPages){
-                    break
-                }
-
+                if(i===numberOfPages)break
                 let className: string = '' 
                 if(i!==currentPage){
                     className = "hide-on-small-screens"
                 }
-
-                elements.push(<Pagination.Item key={i} className={className} active={currentPage===i} href={link+`&page=${i}`}>{i}</Pagination.Item>);
-                
+                elements.push(<Pagination.Item key={`${i} page`} className={className} active={currentPage===i} href={link+`&page=${i}`}>{i}</Pagination.Item>);
             }
             elements.push(<Pagination.Ellipsis className="show-on-small-screens" />)
-            
             return elements
-
-        }
-
-        
+        }  
     }
   
 
     return (
-        <div className="footer">
-            <Pagination >
-                <Pagination.First disabled={currentPage===1} href={link+`&page=${1}`} />
-                <Pagination.Prev disabled={currentPage===1} href={link+`&page=${currentPage-1}`} />
-                <Pagination.Item active={currentPage===1}  href={link+`&page=${1}`}>{1}</Pagination.Item>
-         
+        <Pagination >
+            <Pagination.First disabled={currentPage===1} href={link+`&page=${1}`} />
+            <Pagination.Prev disabled={currentPage===1} href={link+`&page=${currentPage-1}`} />
+            <Pagination.Item active={currentPage===1}  href={link+`&page=${1}`}>{1}</Pagination.Item>
+                {restOfPagination()}
+            <Pagination.Item hidden={numberOfPages===1} active={currentPage===numberOfPages}  href={link+`&page=${numberOfPages}`}>{numberOfPages}</Pagination.Item>
+            <Pagination.Next disabled={currentPage===numberOfPages} href={link+`&page=${currentPage-1}`} />
+            <Pagination.Last disabled={currentPage===numberOfPages} href={link+`&page=${20}`} />
+        </Pagination>
 
-                    {restOfPagination()}
-
-                <Pagination.Item hidden={numberOfPages===1} active={currentPage===numberOfPages}  href={link+`&page=${numberOfPages}`}>{numberOfPages}</Pagination.Item>
-                <Pagination.Next disabled={currentPage===numberOfPages} href={link+`&page=${currentPage-1}`} />
-                <Pagination.Last disabled={currentPage===numberOfPages} href={link+`&page=${20}`} />
-            </Pagination>
-        </div>
     )      
 }
 
